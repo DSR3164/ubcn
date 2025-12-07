9e1e76c71d4ee960fd9d318fd88738a0df16014c
Excalibur <104138693+DSR3164@users.noreply.github.com>
2025-12-07 22:38:19 +0700

    feat: done 2

--- 2/base_stations.m
+++ 2/base_stations.m
@@ -3,7 +3,7 @@ clc; clear; close all;
 TxPowerBS = 46; % dBm в 10^4.6 = 39 810,7 больше 1мВт ~ 39.8 Вт
 SectorsCount = 3; % Количество сот
 TxPowerUE = 24; % dBm ~ 0.25 Вт
-AntGainBS = 21; % dBi 
+AntGainBS = 21; % dBi
 PenetrationM = 15; % dB
 IM = 1; % dB
 % Модель распространения сигнала для макросот: COST 231 Hata
@@ -22,10 +22,10 @@ CentersArea = 4000000; % Площадь ТЦ и БЦ, где требуется 
 % Базовые станции с фидерами
 FeederLoss = 3; % db ~ 2dB фидер + 0.4dB МШУ + 0.5dB Джампер
 MIMOGain = 3; % dB
-d = 0:1:3000; % растояние между BS и UE, метры
-ThermalNoise = -174 + 10 * log(BandwidthDL);
-RxSensBS = NoiseFigureBS + ThermalNoise + SINRDL;
-RxSensUE = NoiseFigureUE + ThermalNoise + SINRUL;
+d = 1:1:5000; % растояние между BS и UE, метры
+ThermalNoise = -174 + 10 * log10(BandwidthDL * 1e6);
+RxSensBS = NoiseFigureBS + ThermalNoise + SINRUL;
+RxSensUE = NoiseFigureUE + ThermalNoise + SINRDL;
 
 %% Расчет бюджета восходящего канала
 MAPL_UL = TxPowerUE - FeederLoss + AntGainBS + MIMOGain - RxSensBS - IM - PenetrationM;
@@ -42,111 +42,149 @@ yline(MAPL_UL, 'r--', 'DisplayName', 'MAPL UL', 'LineWidth', 1.5); hold on;
 plot(d, FSPM(d, Carrier), "DisplayName", "FSPM"); hold on;
 plot(d, UMiNLOS(d, Carrier), "DisplayName", "UMiNLOS"); hold on;
 plot(d, Hata(d, Carrier), "DisplayName", "Hata"); hold on;
-plot(d, Walfisch_Ikegami(d, Carrier, 0), "DisplayName", "Walfisch Ikegami");
+plot(d, Walfisch_Ikegami(d, Carrier, 1), "DisplayName", "Walfisch Ikegami LOS"); hold on;
+plot(d, Walfisch_Ikegami(d, Carrier, 0), "DisplayName", "Walfisch Ikegami NLOS");
 xlabel('Distance, meter'); ylabel('dB');
 grid on;
 legend;
 
+figure;
+D = 100:5:500; % Начальное приближение для расчета дальности
+f = linspace(400, 2400, length(D));
+temp = linspace(-100, 100, length(D));
+[T, F] = meshgrid(temp, f);
+Z = hataplane(f, temp, D);
+s = surf(T, F, Z, Z);
+xlabel("Температура, °C");
+ylabel("Частота, Мгц");
+zlabel("Дальность соты, М");
+xlim([min(T(:)) max(T(:))])
+ylim([min(F(:)) max(F(:))])
+zlim([min(Z(:)) max(Z(:))])
+colormap(jet);
+colorbar;
+
 % Дистанции для каждой модели
-distances = [2193, 35, 78, 504];
-models = ["FSPM", "UMiNLOS", "Okumura-Hata", "Walfisch-Ikegami"]; 
+distances = [
+    getDistance(10000:21000, FSPM(10000:21000, Carrier), MAPL_UL, 1), ...
+    getDistance(d, Walfisch_Ikegami(d, Carrier, 1), MAPL_UL, 1), ...
+    getDistance(d, UMiNLOS(d, Carrier), MAPL_UL, 1), ...
+    getDistance(d, Hata(d, Carrier), MAPL_UL, 1), ...
+    getDistance(d, Walfisch_Ikegami(d, Carrier, 0), MAPL_UL, 1), ...
+    ];
+disp(distances);
+models = ["FSPM", "Walfisch-Ikegami LOS", "UMiNLOS", "Okumura-Hata", "Walfisch-Ikegami NLOS"];
 S = 1.95 * distances.^2;
 for i = 1:length(S)
     count_square(CentersArea, S(i), models(i));
 end
 
-function loss = FSPM(distance, f)
-    % Частота в Гц
-    % Дистанция в метрах
+function d = getDistance(distances, loss, MAPL, show)
+diff = abs(loss - MAPL);
+[~, idx] = min(diff);
+d = distances(idx);
+if show
+    fprintf("\nDistance ≈ %.0f; Loss %.2f = %.2f MAPL\n", d, loss(idx), MAPL);
+end
+end
 
-    f = f * 1e9; % Приводим к ГГц
-    c = 300000000; %% Скорость света
-    lambda = f/c; %% Длина волны
-    loss = 20 * log(4*pi*distance/lambda);
+function z = hataplane(f, temp, D)
+k = 1.380649e-23;
+BW = 10e6;
+
+[T, F] = meshgrid(temp, f);
+
+Tkelv = T + 273.15;
+ThermalNoise = 10*log10(k .* Tkelv .* BW) + 30;
+RxSensBS = 2.4 + ThermalNoise + 4;
+MAPL_UL = 24 - 3 + 21 + 3 - RxSensBS - 1 - 15;
+z = zeros(size(F));
+
+for ii = 1:size(F,1)
+    for jj = 1:size(F,2)
+        ft = F(ii,jj);
+        z(ii, jj) = getDistance(D, Hata(D, ft*1e-3), MAPL_UL(ii,jj), 0);
+    end
+end
+end
+
+
+
+function loss = FSPM(distance, f)
+% Частота в Гц
+% Дистанция в метрах
+
+f = f * 1e9; % Приводим к ГГц
+c = 3 * 1e8; % Скорость света
+lambda = c/f; % Длина волны
+loss = 20 * log10(4*pi*distance./lambda);
 end
 
 function loss = UMiNLOS(distance, f)
-    % Частота в ГГц
-    % Дистанция в метрах
+% Частота в ГГц
+% Дистанция в метрах
 
-    loss = 26 * log(f) + 22.7 + 36.7 * log(distance);
+loss = 26 * log10(f) + 22.7 + 36.7 * log10(distance);
 end
 
 function loss = Hata(distance, f)
-    % Для сетей LTE; частоты 150Мгц - 2ГГц
-    % Высота подвеса антенны БС 30 - 200 метров
-    % Высота антенны мобильного устройства 1 - 10 метров
-    % В радиусе от соты 1 - 20 км
-    % Частота в Гц
-    % Дистанция в метрах
+% distance — вектор (м)
+% f — Гц
 
-    hBS = 60; % высота подвеса, метры
-    hms = 1.8; % высота клиентского устройства в метрах
-    
-    % Я выбрал DU
-    a = 3.2 * (log(11.75*hms))^2 - 4.97;
-    if distance >= 1
-        s = 44.9 - 6.55 * log(f);
-    else
-        s = 47.88 + 13.9 * log(f) - 13.9 * log(hBS) * (1 / log(50));
-    end
-    A = 46.3;
-    B = 33.9;
-    Lclutter = 3;
-    
-    loss = A + B * log(f) - 13.82 * log(hBS) - a + s * log(distance) + Lclutter;
+hBS = 60;
+hms = 1.8;
+
+a = 3.2 * (log10(11.75*hms))^2 - 4.97;
+
+mask_far = (distance >= 1);
+mask_near = ~mask_far;
+
+s = zeros(size(distance));
+s(mask_far)  = 44.9 - 6.55 * log10(f);
+s(mask_near) = 47.88 + 13.9 * log10(f) - 13.9 * log10(hBS) * (1 / log10(50));
+
+A = 46.3;
+B = 33.9;
+Lclutter = 3;
+
+loss = A + B .* log10(f) ...
+    - 13.82 * log10(hBS) ...
+    - a ...
+    + s .* log10(distance) ...
+    + Lclutter;
 end
 
 function loss = Walfisch_Ikegami(distance, f, los)
-    % В условиях городской застройки с «манхэттенской» grid-образной архитектурой
-    % частоты 800Мгц - 2ГГц
-    % Высота подвеса антенны БС 4 - 50 метров
-    % Высота антенны мобильного устройства 1 - 3 метров
-    % В радиусе от соты 0.03 - 6 км
-    % Частота в МГц
-    % Дистанция в километрах
-    % LOS - Line of Sight - Зона прямой видимости
+% distance — вектор (м)
+% f — МГц
 
-    f = f * 1e3; % Приводим к МГц
-    d = distance / 1e3; % Приводим к Км
-    hBS = 30; % высота подвеса, метры
-    hms = 1.8; % высота клиентского устройства в метрах
-    h = 80; % средняя высота зданий, метры, среднее по 180 самым высоким зданиям НСК
-    w = 15; % средняя ширина улиц, метры
-    b = 30; % среднее растояние между зданиями , метры
-    fi = 40; % средний угол между направлением распространения сигнала и улицей, градусы
+f = f * 1e3;
+d = distance * 1e-3;
 
-    if los
-        loss = 42.6 + 20 * log(f) + 26 * log(d);
-    else 
-        L0 = 32.44 + 20 * log(f) + 20 * log(d);
-        if hBS > h
-            L_bsh = - 18 * log(1 + hBS - h);
-            k_a = 54;
-            k_d = 18;
-        else
-            L_bsh = 0;
-            if d > 0.5
-                k_a = 54 - 0.8 * (hBS - h);
-            else
-                k_a = 54 - 0.8 * (hBS - h) * d / 0.5;
-            end
-            k_d = 18 - 15 * (hBS-h) / h;
-        end
-        k_f = - 4 + 0.7 * (f / 925 - 1);
-        L1 = L_bsh + k_a + k_d * log(d) + k_f * log(f) - 9 * log(b);
-        L_ori = 2.5 + 0.075 * (fi - 35);
-        L2 = -16.9 - 10 * log(w) + 10 * log(f) + 20 * log(h - hms) + L_ori;
-        if L1 + L2 > 0
-            loss = L0 + L1 + L2;
-        else
-            loss = L0;
-        end
-    end 
+hBS = 30;
+hms = 1.8;
+h = 30;
+w = 15;
+b = 30;
+fi = 40;
+
+if los
+    loss = 42.6 + 20*log10(f) + 26*log10(d);
+else
+    L0 = 32.44 + 20*log10(f) + 20*log10(d);
+    L_ori = 2.5 + 0.075*(fi - 35);
+    L2 = -16.9 - 10*log10(w) + 10*log10(f) + 20*log10(h - hms) + L_ori;
+    k_a = 54 - 0.8*(hBS - h) .* min(d/0.5, 1);
+    k_d = 18 - 15*(hBS - h)/h;
+    k_f = -4 + 0.7*(f/925 - 1);
+    L1 = k_a + k_d*log10(d) + k_f*log10(f) - 9*log10(b);
+    L_r = L1 + L2;
+    L_r(L_r < 0) = 0;
+    loss = L0 + L_r;
+end
 end
 
 function count = count_square(area, square, model)
-    count = ceil(area / square);
-    fprintf("Количество БС на %dкв.м. должно быть не меньше %d штук по модели %s\n", area, count, model);
-    fprintf("\tРассчитанная площадь БС = %.2fкв.м.\n", square);
+count = ceil(area / square);
+fprintf("%d по модели %s ≈ %.4f км^2\n", count, model, square*1e-6);
 end
